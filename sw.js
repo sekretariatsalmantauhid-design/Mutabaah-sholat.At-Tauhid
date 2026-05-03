105788a65ff478f4b710a4385240e1119572971d
SalmanDesaign-er <sekretariat.salmantauhid@gmail.com>
2026-05-03 13:21:42 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UBAH ANGKA INI SETIAP KALI BOS UPDATE index.html (Misal: v2, v3, v4)
-const CACHE_NAME = 'mutabaah-harian-v2.2'; 
+const CACHE_NAME = 'mini-obs-v4'; 
 
 const urlsToCache = [
  './',
